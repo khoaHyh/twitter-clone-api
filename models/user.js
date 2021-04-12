78ccbfa863e62d7177aa8eb4bf99ddc8f7ed41c2
khoaHyh <khoahuynh.toronto@gmail.com>
2021-04-12 16:52:44 -0400

    Update npm packages and userSchema

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, dropDups: true },
-  password: { type: String },
-  joined: { type: Date, default: new Date() },
+  password: { type: String, required: true },
+  created: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("user", userSchema);
--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const session = require("express-session");
-const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const MongoStore = require("connect-mongo");
 const passport = require("passport");
+const connectDB = require("./db/db");
+
+// Connect to MongoDB
+connectDB();
+
+// Implement a Root-Level Request Logger Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
